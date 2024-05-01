1fdeb1683d48224092d82caa5c63a85ccad3194a
Chirag <chiragmahajan26.cm@gmail.com>
2024-05-01 14:01:22 +0530

    added bakcend files

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const app = require("express");
+const http=require('http').Server(app);
+
+const mongoose=require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/shoplify")
+
+const User=require('./models/userModel')
+
+async function insert(){
+    User.create({
+        name: 'Chirag',
+        email: 'chiragmahajan26.cm@gmail.com'
+    });
+}
+insert();
+
+http.listen(3000, function(){
+    console.log('Server is running');
+});
