5654781df97d4cfc7ce44533e073f0c8c10d7e22
Abc <pupnejachirag109770@gmail.com>
2024-05-07 00:46:05 +0530

    Order APIs completed

--- backend/index.js
+++ backend/index.js
@@ -29,8 +29,10 @@ const errorMiddleware = require("./middleware/error");
 //Routes
 const products = require('./routes/productRoute');
 const user= require('./routes/userRoute');
+const order=require('./routes/orderRoute');
 app.use('/api/v1', products);
 app.use('/api/v1',user);
+app.use('/api/v1',order);
 
 app.use(errorMiddleware);
 
